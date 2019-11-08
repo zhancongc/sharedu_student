92eecc83b012b04bc85ad974c9d48a716378687d
詹聪聪 <1227753320@qq.com>
2019-11-08 18:33:50 +0800

    package

---
+++ unpackage/dist/build/mp-weixin/pages/basics/home.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["pages/basics/home"],{1652:function(n,o,e){"use strict";e.r(o);var t=e("c2ae"),c=e("29f6");for(var a in c)"default"!==a&&function(n){e.d(o,n,function(){return c[n]})}(a);e("a710");var l=e("2877"),u=Object(l["a"])(c["default"],t["a"],t["b"],!1,null,null,null);o["default"]=u.exports},"29f6":function(n,o,e){"use strict";e.r(o);var t=e("2fc1"),c=e.n(t);for(var a in t)"default"!==a&&function(n){e.d(o,n,function(){return t[n]})}(a);o["default"]=c.a},"2fc1":function(n,o,e){"use strict";Object.defineProperty(o,"__esModule",{value:!0}),o.default=void 0;var t={name:"basics",data:function(){return{elements:[{title:"布局",name:"layout",color:"cyan",cuIcon:"newsfill"},{title:"背景",name:"background",color:"blue",cuIcon:"colorlens"},{title:"文本",name:"text",color:"purple",cuIcon:"font"},{title:"图标 ",name:"icon",color:"mauve",cuIcon:"cuIcon"},{title:"按钮",name:"button",color:"pink",cuIcon:"btn"},{title:"标签",name:"tag",color:"brown",cuIcon:"tagfill"},{title:"头像",name:"avatar",color:"red",cuIcon:"myfill"},{title:"进度条",name:"progress",color:"orange",cuIcon:"icloading"},{title:"边框阴影",name:"shadow",color:"olive",cuIcon:"copy"},{title:"加载",name:"loading",color:"green",cuIcon:"loading2"}]}},onShow:function(){console.log("success")}};o.default=t},"864e":function(n,o,e){},a710:function(n,o,e){"use strict";var t=e("864e"),c=e.n(t);c.a},c2ae:function(n,o,e){"use strict";var t=function(){var n=this,o=n.$createElement;n._self._c},c=[];e.d(o,"a",function(){return t}),e.d(o,"b",function(){return c})}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'pages/basics/home-create-component',
+    {
+        'pages/basics/home-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("1652"))
+        })
+    },
+    [['pages/basics/home-create-component']]
+]);                
