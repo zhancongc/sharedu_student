3e7a390f64033a6818207d120fd048a9dc93a724
詹聪聪 <1227753320@qq.com>
2019-11-12 19:08:29 +0800

    add selector

--- pages/lesson_add/lesson_add.vue
+++ pages/lesson_add/lesson_add.vue
@@ -13,6 +13,14 @@
         	<input placeholder="请选择课程类别"  type="text" maxlength="10" 
         		focus="false" @input="setLessonType"></input>
         </view>
+		<view class="cu-form-group">
+		    <view class="title">课程类别</view>
+		    <picker mode="multiSelector" @change="MultiChange" @columnchange="MultiColumnChange" :value="multiIndex" :range="multiArray">
+		        <view class="picker">
+		            {{multiArray[0][multiIndex[0]]}}，{{multiArray[1][multiIndex[1]]}}，{{multiArray[2][multiIndex[2]]}}
+		        </view>
+		    </picker>
+		</view>
         <view class="cu-form-group">
         	<view class="title">课时数</view>
         	<input placeholder="请输入课时数"  type="number" maxlength="9" 
@@ -50,7 +58,69 @@
             setLessonType(e){this.lessonType = e.detail.value},
             setLessonTimes(e){this.lessonTimes = e.detail.value},
             setLessonPrice(e){this.lessonPrice = e.detail.value},
-            setLessonIntro(e){this.lessonIntro = e.detail.value}
+            setLessonIntro(e){this.lessonIntro = e.detail.value},
+			MultiColumnChange(e) {
+			    let data = {
+			        multiArray: this.multiArray,
+			        multiIndex: this.multiIndex
+			    };
+			    data.multiIndex[e.detail.column] = e.detail.value;
+			    switch (e.detail.column) {
+			        case 0:
+			            switch (data.multiIndex[0]) {
+			                case 0:
+			                    data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+			                    data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+			                    break;
+			                case 1:
+			                    data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+			                    data.multiArray[2] = ['鲫鱼', '带鱼'];
+			                    break;
+			            }
+			            data.multiIndex[1] = 0;
+			            data.multiIndex[2] = 0;
+			            break;
+			        case 1:
+			            switch (data.multiIndex[0]) {
+			                case 0:
+			                    switch (data.multiIndex[1]) {
+			                        case 0:
+			                            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+			                            break;
+			                        case 1:
+			                            data.multiArray[2] = ['蛔虫'];
+			                            break;
+			                        case 2:
+			                            data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+			                            break;
+			                        case 3:
+			                            data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+			                            break;
+			                        case 4:
+			                            data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+			                            break;
+			                    }
+			                    break;
+			                case 1:
+			                    switch (data.multiIndex[1]) {
+			                        case 0:
+			                            data.multiArray[2] = ['鲫鱼', '带鱼'];
+			                            break;
+			                        case 1:
+			                            data.multiArray[2] = ['青蛙', '娃娃鱼'];
+			                            break;
+			                        case 2:
+			                            data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+			                            break;
+			                    }
+			                    break;
+			            }
+			            data.multiIndex[2] = 0;
+			            break;
+			    }
+			    this.multiArray = data.multiArray;
+			    this.multiIndex = data.multiIndex;
+			},
 		}
 	}
 </script>
--- pages/store_create/store_create.vue
+++ pages/store_create/store_create.vue
@@ -1,114 +1,114 @@
 <template>
-	<view>
-		<cu-custom bgColor="bg-gradual-pink" :isBack="true">
-			<block slot="backText">返回</block><block slot="content">店铺入驻</block>
-		</cu-custom>
-		<view class="cu-form-group margin-top">
-			<view class="title">邀请码</view>
-			<input placeholder="请输入代理商提供的邀请码"  type="text" maxlength="10" 
-				focus="false" @input="setInvitationCode"></input>
-		</view>
-		<view class="cu-form-group">
-			<view class="title">商户名称</view>
-			<input placeholder="请输入商户名称"  type="text" maxlength="20"
-				focus="true" @input="setStoreName"></input>
-		</view>
-		<view class="cu-form-group">
-			<view class="title">详细地址</view>
-			<input placeholder="请输入商户地址"  type="text" maxlength="30" v-model="storeAddress"
-				focus="true" @input="setStoreAddress"></input>
-			<text class='cuIcon-locationfill text-orange' @click="getLocation"></text>
-		</view>
-		<view class="cu-form-group">
-			<view class="title">负责人</view>
-			<input placeholder="请输入商户主要负责人"  type="text" maxlength="12"
-				focus="true" @input="setManager"></input>
-		</view>
-		<view class="cu-form-group">
-			<view class="title">联系手机</view>
-			<input placeholder="请输入负责人手机号码"  type="number" maxlength="11"
-				focus="true" @input="setManagerPhone"></input>
-		</view>
-		<view class="cu-bar bg-white margin-top">
-			<view class="action">
-				上传营业执照图片
-			</view>
-			<view class="action">
-				{{licenseImageList.length}}/4
-			</view>
-		</view>
-		<view class="cu-form-group">
-			<view class="grid col-4 grid-square flex-sub">
-				<view class="bg-img" v-for="(item,index) in licenseImageList" :key="index" @tap="ViewImage" :data-url="licenseImageList[index]">
-				 <image :src="licenseImageList[index]" mode="aspectFill"></image>
-					<view class="cu-tag bg-red" @tap.stop="DelImg" :data-index="index">
-						<text class='cuIcon-close'></text>
-					</view>
-				</view>
-				<view class="solids" @tap="ChooseImage" v-if="licenseImageList.length<4">
-					<text class='cuIcon-cameraadd'></text>
-				</view>
-			</view>
-		</view>
-        <view class="cu-bar bg-white margin-top">
-        	<view class="action">
-        		上传商户经营场所图片
-        	</view>
-        	<view class="action">
-        		{{storeImageList.length}}/4
-        	</view>
+    <view>
+        <cu-custom bgColor="bg-gradual-pink" :isBack="true">
+            <block slot="backText">返回</block><block slot="content">店铺入驻</block>
+        </cu-custom>
+        <view class="cu-form-group margin-top">
+            <view class="title">邀请码</view>
+            <input placeholder="请输入代理商提供的邀请码"  type="text" maxlength="10" 
+                focus="false" @input="setInvitationCode"></input>
         </view>
         <view class="cu-form-group">
-        	<view class="grid col-4 grid-square flex-sub">
-        		<view class="bg-img" v-for="(item,index) in storeImageList" :key="index" @tap="ViewImage" :data-url="storeImageList[index]">
-        		 <image :src="storeImageList[index]" mode="aspectFill"></image>
-        			<view class="cu-tag bg-red" @tap.stop="DelImg" :data-index="index">
-        				<text class='cuIcon-close'></text>
-        			</view>
-        		</view>
-        		<view class="solids" @tap="ChooseImage" v-if="storeImageList.length<4">
-        			<text class='cuIcon-cameraadd'></text>
-        		</view>
-        	</view>
+            <view class="title">商户名称</view>
+            <input placeholder="请输入商户名称"  type="text" maxlength="20"
+                focus="true" @input="setStoreName"></input>
         </view>
-		<view class="padding flex flex-direction">
-			<button class="cu-btn bg-mauve margin-tb-sm lg" @click="createCommit">提交</button>
-		</view>
-	</view>
+        <view class="cu-form-group">
+            <view class="title">详细地址</view>
+            <input placeholder="请输入商户地址"  type="text" maxlength="30" v-model="storeAddress"
+                focus="true" @input="setStoreAddress"></input>
+            <text class='cuIcon-locationfill text-orange' @click="getLocation"></text>
+        </view>
+        <view class="cu-form-group">
+            <view class="title">负责人</view>
+            <input placeholder="请输入商户主要负责人"  type="text" maxlength="12"
+                focus="true" @input="setManager"></input>
+        </view>
+        <view class="cu-form-group">
+            <view class="title">联系手机</view>
+            <input placeholder="请输入负责人手机号码"  type="number" maxlength="11"
+                focus="true" @input="setManagerPhone"></input>
+        </view>
+        <view class="cu-bar bg-white margin-top">
+            <view class="action">
+                上传营业执照图片
+            </view>
+            <view class="action">
+                {{licenseImageList.length}}/4
+            </view>
+        </view>
+        <view class="cu-form-group">
+            <view class="grid col-4 grid-square flex-sub">
+                <view class="bg-img" v-for="(item,index) in licenseImageList" :key="index" @tap="ViewImage" :data-url="licenseImageList[index]">
+                 <image :src="licenseImageList[index]" mode="aspectFill"></image>
+                    <view class="cu-tag bg-red" @tap.stop="DelImg" :data-index="index">
+                        <text class='cuIcon-close'></text>
+                    </view>
+                </view>
+                <view class="solids" @tap="ChooseImage" v-if="licenseImageList.length<4">
+                    <text class='cuIcon-cameraadd'></text>
+                </view>
+            </view>
+        </view>
+        <view class="cu-bar bg-white margin-top">
+            <view class="action">
+                上传商户经营场所图片
+            </view>
+            <view class="action">
+                {{storeImageList.length}}/4
+            </view>
+        </view>
+        <view class="cu-form-group">
+            <view class="grid col-4 grid-square flex-sub">
+                <view class="bg-img" v-for="(item,index) in storeImageList" :key="index" @tap="ViewImage" :data-url="storeImageList[index]">
+                 <image :src="storeImageList[index]" mode="aspectFill"></image>
+                    <view class="cu-tag bg-red" @tap.stop="DelImg" :data-index="index">
+                        <text class='cuIcon-close'></text>
+                    </view>
+                </view>
+                <view class="solids" @tap="ChooseImage" v-if="storeImageList.length<4">
+                    <text class='cuIcon-cameraadd'></text>
+                </view>
+            </view>
+        </view>
+        <view class="padding flex flex-direction">
+            <button class="cu-btn bg-mauve margin-tb-sm lg" @click="createCommit">提交</button>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				invitationCode: "",
-				owner: "",
-				storeName: "",
-				storeAddress: "",
-				manger: "",
-				managerPhone: "",
-				licenseImageList: [],
+    export default {
+        data() {
+            return {
+                invitationCode: "",
+                owner: "",
+                storeName: "",
+                storeAddress: "",
+                manger: "",
+                managerPhone: "",
+                licenseImageList: [],
                 storeImageList: []
-			}
-		},
-		methods: {
-			setInvitationCode(e) {this.invitationCode = e.detail.value},
-			setStoreName(e) {this.storeName = e.detail.value},
-			setStoreAddress(e) {this.storeAddress = e.detail.value},
-			getLocation(e) {
-				var that = this;
-				uni.chooseLocation({
-				  success: function (res) {
-					if (res.errMsg == "chooseLocation:ok"){
-						//console.log(res)
-						that.storeAddress = res.address
-						that.$forceUpdate()
-					}
-				  }
-				})
-			},
-			setManager(e) {this.manger = e.detail.value},
-			setManagerPhone(e) {this.managerPhone = e.detail.value},
+            }
+        },
+        methods: {
+            setInvitationCode(e) {this.invitationCode = e.detail.value},
+            setStoreName(e) {this.storeName = e.detail.value},
+            setStoreAddress(e) {this.storeAddress = e.detail.value},
+            getLocation(e) {
+                var that = this;
+                uni.chooseLocation({
+                  success: function (res) {
+                    if (res.errMsg == "chooseLocation:ok"){
+                        //console.log(res)
+                        that.storeAddress = res.address
+                        that.$forceUpdate()
+                    }
+                  }
+                })
+            },
+            setManager(e) {this.manger = e.detail.value},
+            setManagerPhone(e) {this.managerPhone = e.detail.value},
             ChooseImage(e) {
                     uni.chooseImage({
                             count: 4, //默认9
@@ -142,23 +142,23 @@
                             }
                     })
             },
-			createCommit(e){
-				uni.request({
-					method: "POST",
-					header: {},
-					url: "",
-					data: {},
-					success(){},
-					fail(){},
-					complete(){},
-				})
-			}
-		}
-	}
+            createCommit(e){
+                uni.request({
+                    method: "POST",
+                    header: {},
+                    url: "",
+                    data: {},
+                    success(){},
+                    fail(){},
+                    complete(){},
+                })
+            }
+        }
+    }
 </script>
 
 <style>
 .cu-form-group .title {
-	min-width: calc(4em + 15px);
+    min-width: calc(4em + 15px);
 }
 </style>
