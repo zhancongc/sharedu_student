c019403eb5489313d93f143ce252492f7c80045f
詹聪聪 <1227753320@qq.com>
2019-11-26 18:47:53 +0800

    add tutor card

--- pages/tutors/tutors.vue
+++ pages/tutors/tutors.vue
@@ -1,6 +1,19 @@
 <template>
 	<view>
-		
+		<view class="grid col-2 padding-sm">
+			<view class="padding-sm">
+				<view class="bg-gradual-red padding radius text-center shadow-blur">
+					<view class="text-lg">魅红</view>
+					<view class="margin-top-sm text-Abc">#f43f3b - #ec008c</view>
+				</view>
+			</view>
+			<view class="padding-sm">
+				<view class="bg-gradual-red padding radius text-center shadow-blur">
+					<view class="text-lg">魅红</view>
+					<view class="margin-top-sm text-Abc">#f43f3b - #ec008c</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
--- unpackage/dist/dev/mp-weixin/pages/tutors/tutors.js
+++ unpackage/dist/dev/mp-weixin/pages/tutors/tutors.js
@@ -126,6 +126,19 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 var _default =
 {
   data: function data() {
